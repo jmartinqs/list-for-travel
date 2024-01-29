0d80a4857b76b601a3356a4ca8b50f005628960e
Martin Quevedo <martinqs@live.com.ar>
2024-01-29 11:04:08 -0300

    sorting items

--- src/App.js
+++ src/App.js
@@ -84,26 +84,48 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items = [], onDeleteItem, onToggleItems }) {
-  useEffect(() => {
-    console.log("PackingList has been re-rendered with updated items:", items);
-  }, [items]);
+function PackingList({ items, onDeleteItem, onToggleItems }) {
+  const [sortedBy, setSortedBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortedBy === "input") sortedItems = items;
+
+  if (sortedBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortedBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
+  // useEffect(() => {
+  //   console.log("PackingList has been re-rendered with updated items:", items);
+  // }, [items]);
 
   return (
-    <>
-      <div className="list">
-        <ul>
-          {items.map((item) => (
-            <Item
-              item={item}
-              onDeleteItem={onDeleteItem}
-              onToggleItems={onToggleItems}
-              key={item.id}
-            />
-          ))}
-        </ul>
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItems={onToggleItems}
+            key={item.id}
+          />
+        ))}
+      </ul>
+
+      <div className="actions">
+        <select value={sortedBy} onChange={(e) => setSortedBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
       </div>
-    </>
+    </div>
   );
 }
 
